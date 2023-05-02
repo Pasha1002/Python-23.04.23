1e6ba0331eba54ddf4d47ee8c6863b8e6824ced9
Pasha1002 <kanavetspavel2001@gmail.com>
2023-05-02 19:04:55 +0300

    tusk dune

---
+++ Python_23.04.23.py
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+try:
+    with open("file.txt", "r") as f:
+        content = f.read()
+    for digit in content.split():
+        if digit.isdigit():
+            print(digit)
+except Exception as e:
+    print("Error", e)
